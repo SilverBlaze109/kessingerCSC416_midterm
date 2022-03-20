4193677b76aa5aeed57616de42b597c11729ba6e
jacobKessinger <jacobkessinger@live.com>
2022-03-19 22:43:21 -0400

    Initial Commit

---
+++ Shared/kessingerCSC416_toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kessingerCSC416_toDoApp.swift
+//  Shared
+//
+//  Created by Jacob on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct kessingerCSC416_toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
